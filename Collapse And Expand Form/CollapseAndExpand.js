6afd2fa456fcd34dbac2be7d8e6533eba76ed65f
SinaKarimi00 <sinakarimi0938@gmail.com>
2021-06-03 08:02:54 +0430

    Collapse And Expand Form

---
+++ Collapse And Expand Form/CollapseAndExpand.js
@@ -0,0 +1,21 @@
+$('#CollapseAll').click(function(){
+    $('.matn').slideUp()
+})
+$('#ExpandAll').click(function(){
+    $('.matn').slideDown()
+})
+$('#ti1').click(function(){
+    $('#Item1').slideToggle()
+})
+$('#ti2').click(function(){
+    $('#Item2').slideToggle()
+})
+$('#ti3').click(function(){
+    $('#Item3').slideToggle()
+})
+$('#ti4').click(function(){
+    $('#Item4').slideToggle()
+})
+$('#ti5').click(function(){
+    $('#Item5').slideToggle()
+})
